b83c21a03453fbd0e4e634ff5b22f741417f763b
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-25 16:02:57 +0700

    display address on search edit text

--- app/src/main/java/com/nguyendinhdoan/userapp/activity/UserActivity.java
+++ app/src/main/java/com/nguyendinhdoan/userapp/activity/UserActivity.java
@@ -1,10 +1,14 @@
 package com.nguyendinhdoan.userapp.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,19 +19,40 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.nguyendinhdoan.userapp.R;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class UserActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback,
+        View.OnTouchListener {
+
+    private static final String TAG = "USER_ACTIVITY";
+    private static final int ORIGIN_AUTOCOMPLETE_REQUEST_CODE = 9000;
+    private static final int DESTINATION_AUTOCOMPLETE_REQUEST_CODE = 9001;
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private EditText originEditText;
+    private EditText destinationEditText;
 
     public static Intent start(Context context) {
         return new Intent(context, UserActivity.class);
@@ -41,45 +66,60 @@ public class UserActivity extends AppCompatActivity
         initViews();
         setupUI();
         addEvents();
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-
-        //
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        supportMapFragment.getMapAsync(this);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void addEvents() {
-
+        navigationView.setNavigationItemSelectedListener(this);
+        originEditText.setOnTouchListener(this);
+        destinationEditText.setOnTouchListener(this);
     }
 
     private void initViews() {
         toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        originEditText = findViewById(R.id.origin_edit_text);
+        destinationEditText = findViewById(R.id.destination_edit_text);
     }
 
     private void setupUI() {
         setupToolbar();
+        setupNavigationView();
+        setupGoogleMap();
+        setupPlacesAPI();
+    }
+
+    private void setupPlacesAPI() {
+        Places.initialize(this, getString(R.string.google_api_key));
+    }
+
+    private void setupGoogleMap() {
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (supportMapFragment != null) {
+            supportMapFragment.getMapAsync(this);
+        }
+    }
+
+    private void setupNavigationView() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
     }
 
-    private void setupToolbar() {
-        setSupportActionBar(toolbar);
-    }
-
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -87,19 +127,14 @@ public class UserActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.user, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -109,7 +144,7 @@ public class UserActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -127,8 +162,7 @@ public class UserActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
@@ -137,11 +171,114 @@ public class UserActivity extends AppCompatActivity
 
         googleMap.addMarker(
                 new MarkerOptions().position(new LatLng(21.1541804, 105.7403176))
-                .title("Ha Noi")
+                        .title("Ha Noi")
         );
 
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                 new LatLng(21.1541804, 105.7403176), 15.0f
         ));
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        // user touch origin edit text
+        if (v.getId() == R.id.origin_edit_text) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN: {
+                    initAutoCompleteSearchOrigin();
+                    break;
+                }
+                case MotionEvent.ACTION_UP: {
+                    v.performClick();
+                    break;
+                }
+                default:
+                    break;
+            }
+        } else if (v.getId() == R.id.destination_edit_text) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN: {
+                    initAutoCompleteSearchDestination();
+                    break;
+                }
+                case MotionEvent.ACTION_UP: {
+                    v.performClick();
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    private void initAutoCompleteSearchDestination() {
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS);
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this);
+        startActivityForResult(intent, DESTINATION_AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    private void initAutoCompleteSearchOrigin() {
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS);
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this);
+        startActivityForResult(intent, ORIGIN_AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ORIGIN_AUTOCOMPLETE_REQUEST_CODE) {
+
+            if (resultCode == RESULT_OK && data != null) {
+
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                String origin = place.getName();
+                Log.d(TAG, "origin place address: " + place.getAddress());
+                Log.d(TAG, "origin place name: " + place.getName());
+                // set address for origin edit text
+                originEditText.setText(origin);
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                Status status = Autocomplete.getStatusFromIntent(Objects.requireNonNull(data));
+                Log.d(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+                showSnackBar(getString(R.string.user_cancel_operation));
+            }
+
+        } else if (requestCode == DESTINATION_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK && data != null) {
+
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                String destination = place.getName();
+                Log.d(TAG, "origin place address: " + place.getAddress());
+                Log.d(TAG, "origin place name: " + place.getName());
+                // set address for destination edit text
+                destinationEditText.setText(destination);
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                Status status = Autocomplete.getStatusFromIntent(Objects.requireNonNull(data));
+                Log.d(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+                showSnackBar(getString(R.string.user_cancel_operation));
+            }
+        }
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT);
+        View view = snackbar.getView();
+        view.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+        TextView textSnack = view.findViewById(android.support.design.R.id.snackbar_text);
+        textSnack.setTextColor(getResources().getColor(R.color.colorBlack));
+        snackbar.show();
     }
 }
